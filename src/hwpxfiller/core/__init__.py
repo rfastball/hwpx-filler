977b392045ca704a33868d16a360b920a46472c6
rfastball <rfastball@gmail.com>
2026-08-10 10:17:18 +0900

    refactor(api): 구체 어댑터 루트 재수출 제거 (#556)
    Closes #555

--- src/hwpxfiller/core/__init__.py
+++ src/hwpxfiller/core/__init__.py
@@ -2,14 +2,12 @@ from __future__ import annotations
 
 from .engine import GenerateResult, HwpxEngine
 from .fields import FieldDocument
-from hwpxcore.package import HwpxPackage
 from hwpxcore.validate import ValidationReport, validate
 
 __all__ = [
     "HwpxEngine",
     "GenerateResult",
     "FieldDocument",
-    "HwpxPackage",
     "ValidationReport",
     "validate",
 ]
